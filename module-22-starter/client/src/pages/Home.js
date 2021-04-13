4744210d33bde830c1505490b3177a100a088138
Micah Pellum <pellummicah@gmail.com>
2021-04-13 09:38:03 -0600

    added state uses for products

---
+++ module-22-starter/client/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+    </div>
+  );
+};
+
+  return (
+    <div className="container">
+      <CategoryMenu setCategory={setCategory} />
+      <ProductList currentCategory={currentCategory} />
+    </div>
+  );
+
+
+export default Home;
