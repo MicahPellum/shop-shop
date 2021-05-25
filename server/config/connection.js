55d543b9fdd97bc5495a6749794d2bf2cc4287f9
Micah Pellum <pellummicah@gmail.com>
2021-05-25 08:37:01 -0600

    adding mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://MicahPellum:<Hello123>@cluster0.r2ord.mongodb.net/shop_shop?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
